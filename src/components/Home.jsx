9b48454446e93768ecfd844f5c61ee26322e7473
gpchandrarao <128778603+Gpchandrarao@users.noreply.github.com>
2024-03-31 13:03:35 +0530

    Home component completed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,13 +1,33 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Header from "./Header";
+import banner from "../assets/Banner.svg";
+import "../styles/Home.css";
 
 const Home = () => {
   return (
-    <div>
+    <div className="">
       <Header />
       <Navbar />
-      <div className=""></div>
+      <div
+        className="home-container"
+        style={{
+          backgroundImage: `url(${banner})`,
+          height: "100vh",
+          width: "100%",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <h1 className="main-heading">
+          Discover Exciting Eventss Happening Near You - Stay Tuned for Updates!
+        </h1>
+        <p className="sub-heading">
+          Dorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc
+          vulputate libero et velit interdum ac aliquet odio mattis. Class
+          aptent taciti sociosqu ad litora torquent per conubia nostra, per
+        </p>
+      </div>
     </div>
   );
 };
